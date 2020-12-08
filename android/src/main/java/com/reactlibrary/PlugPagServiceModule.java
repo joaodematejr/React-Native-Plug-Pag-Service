336eebd7b74576a4dcad9baf253754861e83742c
João Dematé Jr <joaodematejr@gmail.com>
2020-12-07 23:16:19 -0300

    Update PlugPagServiceModule.java

--- android/src/main/java/com/reactlibrary/PlugPagServiceModule.java
+++ android/src/main/java/com/reactlibrary/PlugPagServiceModule.java
@@ -1,6 +1,9 @@
 package com.reactlibrary;
 
+import android.util.Log;
+
 import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
@@ -11,6 +14,11 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import br.com.uol.pagseguro.plugpagservice.wrapper.PlugPag;
 import br.com.uol.pagseguro.plugpagservice.wrapper.PlugPagActivationData;
@@ -22,15 +30,12 @@ import br.com.uol.pagseguro.plugpagservice.wrapper.PlugPagTransactionResult;
 public class PlugPagServiceModule extends ReactContextBaseJavaModule {
 
     private final ReactApplicationContext reactContext;
-    private ArrayList<PlugPagAppIdentificationWrapper> appIdentifications;
-    private ArrayList<PlugPagWrapper> plugPags;
+    private PlugPagAppIdentification appIdentification;
+    private PlugPag plugPag;
 
     public PlugPagServiceModule(ReactApplicationContext reactContext) {
         super(reactContext);
         this.reactContext = reactContext;
-
-        appIdentifications = new ArrayList<>();
-        plugPags = new ArrayList<>();
     }
 
     @Override
@@ -42,138 +47,132 @@ public class PlugPagServiceModule extends ReactContextBaseJavaModule {
     @Override
     public Map<String, Object> getConstants() {
         final Map<String, Object> constants = new HashMap<>();
-        constants.put("PlugPag", Collections.unmodifiableMap(new HashMap<String, Object>() {{
-            put("TYPE_CREDITO", PlugPag.TYPE_CREDITO);
-            put("TYPE_DEBITO", PlugPag.TYPE_DEBITO);
-            put("TYPE_VOUCHER", PlugPag.TYPE_VOUCHER);
 
-            put("INSTALLMENT_TYPE_A_VISTA", PlugPag.INSTALLMENT_TYPE_A_VISTA);
-            put("INSTALLMENT_TYPE_PARC_VENDEDOR", PlugPag.INSTALLMENT_TYPE_PARC_VENDEDOR);
-            put("INSTALLMENT_TYPE_PARC_COMPRADOR", PlugPag.INSTALLMENT_TYPE_PARC_COMPRADOR);
+        constants.put("PAYMENT_CREDITO", PlugPag.TYPE_CREDITO);
+        constants.put("PAYMENT_DEBITO", PlugPag.TYPE_DEBITO);
+        constants.put("PAYMENT_VOUCHER", PlugPag.TYPE_VOUCHER);
 
-            put("OPERATION_ABORT", PlugPag.OPERATION_ABORT);
-            put("OPERATION_ABORTED", PlugPag.OPERATION_ABORTED);
-            put("OPERATION_ACTIVATE", PlugPag.OPERATION_ACTIVATE);
-            put("OPERATION_CALCULATE_INSTALLMENTS", PlugPag.OPERATION_CALCULATE_INSTALLMENTS);
-            put("OPERATION_CHECK_AUTHENTICATION", PlugPag.OPERATION_CHECK_AUTHENTICATION);
-            put("OPERATION_DEACTIVATE", PlugPag.OPERATION_DEACTIVATE);
-            put("OPERATION_GET_APPLICATION_CODE", PlugPag.OPERATION_GET_APPLICATION_CODE);
-            put("OPERATION_GET_LIB_VERSION", PlugPag.OPERATION_GET_LIB_VERSION);
-            put("OPERATION_GET_READER_INFOS", PlugPag.OPERATION_GET_READER_INFOS);
-            put("OPERATION_GET_USER_DATA", PlugPag.OPERATION_GET_USER_DATA);
-            put("OPERATION_HAS_CAPABILITY", PlugPag.OPERATION_HAS_CAPABILITY);
-            put("OPERATION_INVALIDATE_AUTHENTICATION", PlugPag.OPERATION_INVALIDATE_AUTHENTICATION);
-            put("OPERATION_NFC_ABORT", PlugPag.OPERATION_NFC_ABORT);
-            put("OPERATION_NFC_READ", PlugPag.OPERATION_NFC_READ);
-            put("OPERATION_NFC_WRITE", PlugPag.OPERATION_NFC_WRITE);
-            put("OPERATION_PAYMENT", PlugPag.OPERATION_PAYMENT);
-            put("OPERATION_PRINT", PlugPag.OPERATION_PRINT);
-            put("OPERATION_QUERY_LAST_APPROVED_TRANSACTION", PlugPag.OPERATION_QUERY_LAST_APPROVED_TRANSACTION);
-            put("OPERATION_REFUND", PlugPag.OPERATION_REFUND);
-            put("OPERATION_REPRINT_CUSTOMER_RECEIPT", PlugPag.OPERATION_REPRINT_CUSTOMER_RECEIPT);
-            put("OPERATION_REPRINT_STABLISHMENT_RECEIPT", PlugPag.OPERATION_REPRINT_STABLISHMENT_RECEIPT);
+        constants.put("INSTALLMENT_TYPE_A_VISTA", PlugPag.INSTALLMENT_TYPE_A_VISTA);
+        constants.put("INSTALLMENT_TYPE_PARC_VENDEDOR", PlugPag.INSTALLMENT_TYPE_PARC_VENDEDOR);
+        constants.put("INSTALLMENT_TYPE_PARC_COMPRADOR", PlugPag.INSTALLMENT_TYPE_PARC_COMPRADOR);
 
-            put("ACTION_POST_OPERATION", PlugPag.ACTION_POST_OPERATION);
-            put("ACTION_PRE_OPERATION", PlugPag.ACTION_PRE_OPERATION);
-            put("ACTION_UPDATE", PlugPag.ACTION_UPDATE);
+        constants.put("OPERATION_ABORT", PlugPag.OPERATION_ABORT);
+        constants.put("OPERATION_ABORTED", PlugPag.OPERATION_ABORTED);
+        constants.put("OPERATION_ACTIVATE", PlugPag.OPERATION_ACTIVATE);
+        constants.put("OPERATION_CALCULATE_INSTALLMENTS", PlugPag.OPERATION_CALCULATE_INSTALLMENTS);
+        constants.put("OPERATION_CHECK_AUTHENTICATION", PlugPag.OPERATION_CHECK_AUTHENTICATION);
+        constants.put("OPERATION_DEACTIVATE", PlugPag.OPERATION_DEACTIVATE);
+        constants.put("OPERATION_GET_APPLICATION_CODE", PlugPag.OPERATION_GET_APPLICATION_CODE);
+        constants.put("OPERATION_GET_LIB_VERSION", PlugPag.OPERATION_GET_LIB_VERSION);
+        constants.put("OPERATION_GET_READER_INFOS", PlugPag.OPERATION_GET_READER_INFOS);
+        constants.put("OPERATION_GET_USER_DATA", PlugPag.OPERATION_GET_USER_DATA);
+        constants.put("OPERATION_HAS_CAPABILITY", PlugPag.OPERATION_HAS_CAPABILITY);
+        constants.put("OPERATION_INVALIDATE_AUTHENTICATION", PlugPag.OPERATION_INVALIDATE_AUTHENTICATION);
+        constants.put("OPERATION_NFC_ABORT", PlugPag.OPERATION_NFC_ABORT);
+        constants.put("OPERATION_NFC_READ", PlugPag.OPERATION_NFC_READ);
+        constants.put("OPERATION_NFC_WRITE", PlugPag.OPERATION_NFC_WRITE);
+        constants.put("OPERATION_PAYMENT", PlugPag.OPERATION_PAYMENT);
+        constants.put("OPERATION_PRINT", PlugPag.OPERATION_PRINT);
+        constants.put("OPERATION_QUERY_LAST_APPROVED_TRANSACTION", PlugPag.OPERATION_QUERY_LAST_APPROVED_TRANSACTION);
+        constants.put("OPERATION_REFUND", PlugPag.OPERATION_REFUND);
+        constants.put("OPERATION_REPRINT_CUSTOMER_RECEIPT", PlugPag.OPERATION_REPRINT_CUSTOMER_RECEIPT);
+        constants.put("OPERATION_REPRINT_STABLISHMENT_RECEIPT", PlugPag.OPERATION_REPRINT_STABLISHMENT_RECEIPT);
 
-            put("APN_SERVICE_CLASS_NAME", PlugPag.APN_SERVICE_CLASS_NAME);
-            put("APN_SERVICE_PACKAGE_NAME", PlugPag.APN_SERVICE_PACKAGE_NAME);
+        constants.put("ACTION_POST_OPERATION", PlugPag.ACTION_POST_OPERATION);
+        constants.put("ACTION_PRE_OPERATION", PlugPag.ACTION_PRE_OPERATION);
+        constants.put("ACTION_UPDATE", PlugPag.ACTION_UPDATE);
 
-            put("AUTHENTICATION_FAILED", PlugPag.AUTHENTICATION_FAILED);
-            put("COMMUNICATION_ERROR", PlugPag.COMMUNICATION_ERROR);
-            put("ERROR_CODE_OK", PlugPag.ERROR_CODE_OK);
-            put("MIN_PRINTER_STEPS", PlugPag.MIN_PRINTER_STEPS);
-            put("NFC_SERVICE_CLASS_NAME", PlugPag.NFC_SERVICE_CLASS_NAME);
-            put("NFC_SERVICE_PACKAGE_NAME", PlugPag.NFC_SERVICE_PACKAGE_NAME);
-            put("NO_PRINTER_DEVICE", PlugPag.NO_PRINTER_DEVICE);
-            put("NO_TRANSACTION_DATA", PlugPag.NO_TRANSACTION_DATA);
-            put("SERVICE_CLASS_NAME", PlugPag.SERVICE_CLASS_NAME);
-            put("SERVICE_PACKAGE_NAME", PlugPag.SERVICE_PACKAGE_NAME);
-            put("SMART_RECHARGE_SERVICE_CLASS_NAME", PlugPag.SMART_RECHARGE_SERVICE_CLASS_NAME);
-            put("SMART_RECHARGE_SERVICE_PACKAGE_NAME", PlugPag.SMART_RECHARGE_SERVICE_PACKAGE_NAME);
+        constants.put("APN_SERVICE_CLASS_NAME", PlugPag.APN_SERVICE_CLASS_NAME);
+        constants.put("APN_SERVICE_PACKAGE_NAME", PlugPag.APN_SERVICE_PACKAGE_NAME);
 
-            put("RET_OK", PlugPag.RET_OK);
-        }}));
+        constants.put("AUTHENTICATION_FAILED", PlugPag.AUTHENTICATION_FAILED);
+        constants.put("COMMUNICATION_ERROR", PlugPag.COMMUNICATION_ERROR);
+        constants.put("ERROR_CODE_OK", PlugPag.ERROR_CODE_OK);
+        constants.put("MIN_PRINTER_STEPS", PlugPag.MIN_PRINTER_STEPS);
+        constants.put("NFC_SERVICE_CLASS_NAME", PlugPag.NFC_SERVICE_CLASS_NAME);
+        constants.put("NFC_SERVICE_PACKAGE_NAME", PlugPag.NFC_SERVICE_PACKAGE_NAME);
+        constants.put("NO_PRINTER_DEVICE", PlugPag.NO_PRINTER_DEVICE);
+        constants.put("NO_TRANSACTION_DATA", PlugPag.NO_TRANSACTION_DATA);
+        constants.put("SERVICE_CLASS_NAME", PlugPag.SERVICE_CLASS_NAME);
+        constants.put("SERVICE_PACKAGE_NAME", PlugPag.SERVICE_PACKAGE_NAME);
+        constants.put("SMART_RECHARGE_SERVICE_CLASS_NAME", PlugPag.SMART_RECHARGE_SERVICE_CLASS_NAME);
+        constants.put("SMART_RECHARGE_SERVICE_PACKAGE_NAME", PlugPag.SMART_RECHARGE_SERVICE_PACKAGE_NAME);
+
+        constants.put("RET_OK", PlugPag.RET_OK);
 
         return constants;
     }
 
     // Cria a identificação do aplicativo
     @ReactMethod
-    public void getAppIdendification(String name, String version, Callback callback) {
-        PlugPagAppIdentificationWrapper appIdentification = new PlugPagAppIdentificationWrapper(name, version);
-        appIdentifications.add(appIdentification);
-        callback.invoke(appIdentification.tag);
-    }
-
-    // Cria a referência do PlugPag
-    @ReactMethod
-    public void getPlugPag(String appId, Callback successCallback, Callback errorCallback) {
-        PlugPagAppIdentificationWrapper appIdWrapper = null;
-        for (PlugPagAppIdentificationWrapper appIdentification: appIdentifications) {
-            if (appIdentification.equals(appId)) {
-                appIdWrapper = appIdentification;
-                break;
-            }
-        }
-        if (appIdWrapper != null) {
-            PlugPagWrapper plugpag = new PlugPagWrapper(reactContext, appIdWrapper.appIdentification);
-            this.plugPags.add(plugpag);
-            successCallback.invoke(plugpag.tag);
-        } else {
-            errorCallback.invoke("Can't find app Identification");
-        }
+    public void setAppIdendification(String name, String version) {
+        appIdentification = new PlugPagAppIdentification(name, version);
+        plugPag = new PlugPag(reactContext, appIdentification);
     }
 
     // Ativa terminal e faz o pagamento
     @ReactMethod
-    public void initializeAndActivatePinpad(String plugPagId, String jsonStr, Callback successCallback, Callback errorCallback) {
-        PlugPagActivationData activationData = JsonParseUtils.getPlugPagActivationDataFromJson(jsonStr);
-        if (activationData != null) {
+    public void initializeAndActivatePinpad(String activationCode, Promise promise) {
+        final PlugPagActivationData activationData = new PlugPagActivationData(activationCode);
 
-            PlugPagWrapper plugPagWrapper = null;
-
-            for (PlugPagWrapper wrapper: plugPags) {
-                if (wrapper.equals(plugPagId)) {
-                    plugPagWrapper = wrapper;
-                    break;
-                }
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Callable<PlugPagInitializationResult> callable = new Callable<PlugPagInitializationResult>() {
+            @Override
+            public PlugPagInitializationResult call() throws Exception {
+                return plugPag.initializeAndActivatePinpad(activationData);
             }
+        };
 
-            if (plugPagWrapper != null) {
-                PlugPagInitializationResult initResult = plugPagWrapper.plugPag.initializeAndActivatePinpad(activationData);
-                successCallback.invoke(initResult.getResult());
-            } else {
-                errorCallback.invoke("Can't find plugPag");
-            }
-        } else {
-            errorCallback.invoke("PlugPagActivationData error");
+        Future<PlugPagInitializationResult> future = executor.submit(callable);
+        executor.shutdown();
+
+        try {
+            PlugPagInitializationResult initResult = future.get();
+
+            final WritableMap map = Arguments.createMap();
+            map.putInt("retCode", initResult.getResult());
+
+            promise.resolve(map);
+        } catch (ExecutionException e) {
+            Log.d("PlugPag", e.getMessage());
+            promise.reject("error", e.getMessage());
+        } catch (InterruptedException e) {
+            Log.d("PlugPag", e.getMessage());
+            promise.reject("error", e.getMessage());
         }
     }
 
     @ReactMethod
-    public void doPayment(String plugPagId, String jsonStr, Callback successCallback, Callback errorCallback) {
-        PlugPagPaymentData paymentData = JsonParseUtils.getPlugPagPaymentDataFromJson(jsonStr);
-        if (paymentData != null) {
-
-            PlugPagWrapper plugPagWrapper = null;
-
-            for (PlugPagWrapper wrapper: plugPags) {
-                if (wrapper.equals(plugPagId)) {
-                    plugPagWrapper = wrapper;
-                    break;
-                }
+    public void doPayment(String jsonStr, Promise promise) {
+        final PlugPagPaymentData paymentData = JsonParseUtils.getPlugPagPaymentDataFromJson(jsonStr);
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Callable<PlugPagTransactionResult> callable = new Callable<PlugPagTransactionResult>() {
+            @Override
+            public PlugPagTransactionResult call() throws Exception {
+                return plugPag.doPayment(paymentData);
             }
-
-            if (plugPagWrapper != null) {
-                PlugPagTransactionResult transactionResult = plugPagWrapper.plugPag.doPayment(paymentData);
-                successCallback.invoke(transactionResult.getResult());
-            } else {
-                errorCallback.invoke("Can't find plugPag");
-            }
-        } else {
-            errorCallback.invoke("PlugPagPaymentData error");
+        };
+        Future<PlugPagTransactionResult> future = executor.submit(callable);
+        executor.shutdown();
+        try {
+            PlugPagTransactionResult transactionResult = future.get();
+            final WritableMap map = Arguments.createMap();
+            map.putInt("code", transactionResult.getResult());
+            map.putString("amount", transactionResult.getAmount());
+            map.putString("bin", transactionResult.getBin());
+            map.putString("cardApplication", transactionResult.getCardApplication());
+            map.putString("cardBrand", transactionResult.getCardBrand());
+            map.putString("errorCode", transactionResult.getErrorCode());
+            map.putString("message", transactionResult.getMessage());
+            map.putString("transactionCode", transactionResult.getTransactionCode());
+            map.putString("transactionId", transactionResult.getTransactionId());
+            promise.resolve(map);
+        } catch (ExecutionException e) {
+            Log.d("PlugPag", e.getMessage());
+            promise.reject("error", e.getMessage());
+        } catch (InterruptedException e) {
+            Log.d("PlugPag", e.getMessage());
+            promise.reject("error", e.getMessage());
         }
     }
 
